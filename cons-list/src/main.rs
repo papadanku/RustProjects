093dbecf8341a5c1f497ca727b8c3934b7f67b32
papadanku <pauldang2@gmail.com>
2024-12-31 17:31:58 -0700

    Learning strong/weak refs

--- cons-list/src/main.rs
+++ cons-list/src/main.rs
@@ -5,20 +5,42 @@ use std::rc::Rc;
 
 #[derive(Debug)]
 enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil,
+    Cons(i32, RefCell<Rc<List>>),
+    Nil
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None
+        }
+    }
 }
 
 fn main() {
-    let value: Rc<RefCell<i32>> = Rc::new(RefCell::new(5));
+    // Create list a
+    let a: Rc<List> = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    let a: Rc<List> = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    let b: List = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c: List = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    *value.borrow_mut() += 10;
+    // Create list b that points to a list in a as the tail-end
+    let b: Rc<List> = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    println!("a after = {a:?}");
-    println!("b after = {b:?}");
-    println!("c after = {c:?}");
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    // Modify list a to point to b, creating a reference cycle.
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
